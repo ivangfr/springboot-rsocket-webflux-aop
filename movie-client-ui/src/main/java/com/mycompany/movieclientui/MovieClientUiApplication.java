7b4cebef0455241999fbd51ceb76cfbc318d259f
Ivan Franchin <ivan.franchin@takeaway.com>
2021-11-02 19:44:56 +0100

    project update - update to spring-native 0.10.5; - update to native-buildtools 0.9.4; - update README.

--- movie-client-ui/src/main/java/com/mycompany/movieclientui/MovieClientUiApplication.java
+++ movie-client-ui/src/main/java/com/mycompany/movieclientui/MovieClientUiApplication.java
@@ -2,7 +2,12 @@ package com.mycompany.movieclientui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.AotProxyHint;
+import org.springframework.nativex.hint.ProxyBits;
 
+@AotProxyHint(
+        targetClass = com.mycompany.movieclientui.controller.MovieClientUiController.class,
+        proxyFeatures = ProxyBits.IS_STATIC)
 @SpringBootApplication
 public class MovieClientUiApplication {
 
