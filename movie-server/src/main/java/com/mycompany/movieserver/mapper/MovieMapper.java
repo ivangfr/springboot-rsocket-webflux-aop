0c18e9df451be21ca45de682f2b4af9cd082cde6
Ivan Franchin <ivan.franchin@takeaway.com>
2022-04-27 08:57:14 +0100

    project update - upgrade to spring-boot 2.6.7; - upgrade to spring-native 0.11.4; - upgrade to native-buildtools 0.9.11; - upgrade to jib 3.2.1; - upgrade to openjdk-image 11.0.14.1; - upgrade to mongo docker image 5.0.7; - add Mapping annotation to ignore some fields in mapper class; - update README.

--- movie-server/src/main/java/com/mycompany/movieserver/mapper/MovieMapper.java
+++ movie-server/src/main/java/com/mycompany/movieserver/mapper/MovieMapper.java
@@ -3,9 +3,13 @@ package com.mycompany.movieserver.mapper;
 import com.mycompany.movieserver.controller.dto.AddMovieRequest;
 import com.mycompany.movieserver.model.Movie;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface MovieMapper {
 
+    @Mapping(target = "likes", ignore = true)
+    @Mapping(target = "dislikes", ignore = true)
+    @Mapping(target = "lastModifiedDate", ignore = true)
     Movie toMovie(AddMovieRequest addMovieRequest);
 }
