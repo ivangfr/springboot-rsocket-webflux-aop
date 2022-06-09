a0dbc73b72b54d4921f2f0ad56280d4f23683ce0
Ivan Franchin <ivan.franchin@takeaway.com>
2022-06-09 07:04:49 +0200

    project upgrade - upgrade to spring-boot 2.7.0; - upgrade to spring-native 0.12.0; - upgrade to openjdk-image 11.0.15; - upgrade to mongo docker image 5.0.9; - upgrade to sockjs-client 1.6.1; - upgrade to moment.js 2.29.3; - add NativeHint to MovieClientUiApplication; - update README.

--- movie-client-ui/src/main/java/com/mycompany/movieclientui/MovieClientUiApplication.java
+++ movie-client-ui/src/main/java/com/mycompany/movieclientui/MovieClientUiApplication.java
@@ -1,13 +1,23 @@
 package com.mycompany.movieclientui;
 
+import com.mycompany.movieclientui.controller.MovieClientUiController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.nativex.hint.AotProxyHint;
+import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.ProxyBits;
+import org.springframework.nativex.hint.TypeHint;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
 
-@AotProxyHint(
-        targetClass = com.mycompany.movieclientui.controller.MovieClientUiController.class,
-        proxyFeatures = ProxyBits.IS_STATIC)
+@NativeHint(
+        aotProxies = @AotProxyHint(
+                targetClass = MovieClientUiController.class,
+                proxyFeatures = ProxyBits.IS_STATIC
+        ),
+        types = @TypeHint(
+                types = TomcatRequestUpgradeStrategy.class
+        )
+)
 @SpringBootApplication
 public class MovieClientUiApplication {
 
